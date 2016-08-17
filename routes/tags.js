6b290d80435d413c3f44292493df62a39a9e5a71
Giovanna Batalha <u14169@academico.cotuca.unicamp.br>
2016-08-17 08:40:55 -0300

    Ajustes

--- routes/tags.js
+++ routes/tags.js
@@ -1,27 +1,4 @@
-<<<<<<< HEAD
-var express = require('express');
-var router = express.Router();
-var Tag   = require('../app/models/tag.js');
 
-/* GET users listing. */
-router.get('/', function(req, res) {
-  Tag.find({},function(err, tags){
-  	console.log("Results", tags);
-    res.json(tags);
-  });
-});
-
-router.post('/', function(req, res) {
-    var newTag = new Tag(req.body);
-      console.log("New Tag: ", newTag);
-      newTag.save(function (err, result){
-      res.send(result);
-      console.log("post Tag - result:", result);
-      console.log("post Tag - err:", err);
-  });
-});
-
-=======
 var express = require('express');
 var router = express.Router();
 var Tag   = require('../app/models/tag.js');
@@ -44,5 +21,4 @@ router.post('/', function(req, res) {
   });
 });
 
->>>>>>> 436ddb90a072701425223ba7b4e730fc3844760b
 module.exports = router;
