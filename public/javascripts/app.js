3707a89f06562bcc70bbf24eba73769e218969ce
Giovanna Batalha <giovanna.batalha03@gmail.com>
2016-09-28 09:59:37 -0300

    Ajuste 28.09.2016

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,16 +3,16 @@ angular.module("app",['ui.router'])
 		console.log($stateProvider, $urlRouterProvider);
 		$urlRouterProvider.otherwise('/');
 		$stateProvider
-			.state('route1',{url:'/route1', templateUrl:'/app/templates/route1.html', controller:'UsersController'})
+			/*.state('route1',{url:'/route1', templateUrl:'/app/templates/route1.html', controller:'UsersController'})*/
 			.state('newUser', {url:'/newUser', templateUrl:'/app/users/templates/newUser.html', controller:'UsersController'})
 			.state('newRecipe', {url:'/newRecipe', templateUrl:'/app/recipes/templates/newRecipe.html', controller:'recipeController'})
-			.state('newComment', {url:'/newComment', templateUrl:'/app/comments/templates/newComment.html', controller:'commentController'})
-			.state('newTag', {url:'/newTag', templateUrl:'/app/tags/templates/newTag.html', controller:'tagController'})
-			.state('newCategory', {url:'/newCategory', templateUrl:'/app/categories/templates/newCategory.html', controller:'categoryController'})
+			/*.state('newComment', {url:'/newComment', templateUrl:'/app/comments/templates/newComment.html', controller:'commentController'})*/
+			/*.state('newTag', {url:'/newTag', templateUrl:'/app/tags/templates/newTag.html', controller:'tagController'})
+			.state('newCategory', {url:'/newCategory', templateUrl:'/app/categories/templates/newCategory.html', controller:'categoryController'})*/
 			.state('login',{url:'/login', templateUrl:'/app/auth/templates/login.html', controller:'loginController'})
-			.state('viewTags',{url:'/tags', templateUrl:'/app/general/list.html', controller:'tagManagerController'})
+			/*.state('viewTags',{url:'/tags', templateUrl:'/app/general/list.html', controller:'tagManagerController'})*/
 			.state('viewRecipes',{url:'/recipes', templateUrl:'/app/general/list.html', controller:'recipeManagerController'})
-			.state('viewCategories',{url:'/categories', templateUrl:'/app/general/list.html', controller:'categoryController'})
+			/*.state('viewCategories',{url:'/categories', templateUrl:'/app/general/list.html', controller:'categoryController'})*/
 
 			.state('viewRecipesByParam',{url:'/:valueParam?param', templateUrl:'/app/general/list.html', controller:'recipeManagerController'});
 
